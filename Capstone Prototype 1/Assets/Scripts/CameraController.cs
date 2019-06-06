fb9dd3353a58fde8a5a6a433c0bcf0445b33d76d
Scott Duman <sbd39@comcast.net>
2019-06-05 22:37:11 -0700

    Created Basic Player Controller and Camera Controller
    Implemented player controller allowing the player to move the player character through wasd or arrow keys and enabling jumping. I also implemented a camera controller so that the player can look around (the player character will move towards wherever the camera is facing).

---
+++ Capstone Prototype 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float rotHorizontal = 10;
+    public float rotVertical = 10;
+
+    private float vertical = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Mouse X")*rotHorizontal + transform.parent.localEulerAngles.y;
+        transform.parent.localEulerAngles = new Vector3(0, horizontal, 0);
+
+        vertical -= Input.GetAxis("Mouse Y")*rotVertical;
+        vertical = Mathf.Clamp(vertical, -60, 60);
+        transform.localEulerAngles = new Vector3(vertical, 0, 0);
+    }
+}
---
+++ Capstone Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    public float jumpForce = 5;
+
+    private Vector3 moveDir;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        moveDir = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = moveDir.y;
+
+        moveDir = transform.right * Input.GetAxis("Horizontal");
+        moveDir += transform.forward * Input.GetAxis("Vertical");
+        moveDir = moveDir.normalized * moveSpeed;
+
+        if (controller.isGrounded)
+        { 
+            if (Input.GetKey(KeyCode.Space)) moveDir.y = jumpForce;
+        }
+        else moveDir.y = y + Physics.gravity.y*Time.deltaTime;
+
+        controller.Move(moveDir * Time.deltaTime);
+
+        Vector3 jump = Vector3.zero;
+    }
+}
