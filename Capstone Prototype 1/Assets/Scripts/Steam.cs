06f2488b4a087e6308c944db5dbe126a6857b36b
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-06-09 19:46:22 -0500

    Super basic prototypes for the water states

---
+++ Capstone Prototype 1/Assets/Scripts/Steam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steam : MonoBehaviour
+{
+    public float RiseSpeed = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if(rb != null)
+        {
+            rb.velocity = new Vector3(0, RiseSpeed, 0);
+            print("TEST");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
