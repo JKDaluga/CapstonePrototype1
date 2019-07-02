6f4c3c0a9f0e517362470d6c5fb871ca7a391059
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-07-01 20:05:17 -0500

    pipe prototype stuff

---
+++ Capstone Prototype 1/Assets/Scripts/Pipe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public Pipe exitPipe;
+    public Transform exitSpot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changePipe(Pipe exit)
+    {
+        exitPipe = exit;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject != null && other.gameObject.tag == "Gas")
+        {
+            other.gameObject.transform.position = exitSpot.position;
+        }
+    }
+}
