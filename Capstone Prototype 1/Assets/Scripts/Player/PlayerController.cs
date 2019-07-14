0d5e664a4de5be390aabd96aa54e270621a98914
Scott Duman <sbd39@comcast.net>
2019-07-14 04:22:51 -0700

    Added Interaction Range to Player Controller

--- Capstone Prototype 1/Assets/Scripts/Player/PlayerController.cs
+++ Capstone Prototype 1/Assets/Scripts/Player/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     CursorManager cursorManager;
+    [SerializeField]
+    float interactDistance = 10;
     GameObject currentTarget = null;
 
     void Update()
     {
-        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out RaycastHit hit))
+        if (Physics.Raycast(Camera.main.ViewportPointToRay(new Vector2(0.5f, 0.5f)), out RaycastHit hit, interactDistance))
         {
             GameObject target = hit.collider.gameObject;
             if (currentTarget == null || !hit.collider.gameObject.Equals(currentTarget))
