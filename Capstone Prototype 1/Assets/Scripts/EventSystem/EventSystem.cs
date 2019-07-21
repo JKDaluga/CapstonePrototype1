2f4f2e7e20690a2fda5e7788ac632beb9e77f50d
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-07-21 14:51:35 -0500

    Created an EventSystem
    The groundwork of an eventsystem that can be used for levers, buttons etc. It can probably even be expanded to things like death and audio triggers

---
+++ Capstone Prototype 1/Assets/Scripts/EventSystem/EventSystem.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EventSystem
+{
+    public class EventSystem : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        static private EventSystem __Current;
+        static public EventSystem Current
+        {
+            get
+            {
+                if (__Current == null)
+                {
+                    __Current = GameObject.FindObjectOfType<EventSystem>();
+                }
+
+                return __Current;
+            }
+        }
+        
+        public delegate void EventListener(EventInfo e);
+        Dictionary<System.Type, List<EventListener>> eventListeners;
+
+        public void RegisterListener(System.Type eventType, EventListener listener)
+        {
+            if (eventListeners == null)
+            {
+                eventListeners = new Dictionary<System.Type, List<EventListener>>();
+            }
+
+            if (eventListeners.ContainsKey(eventType) == false || eventListeners[eventType] == null)
+            {
+                eventListeners[eventType] = new List<EventListener>();
+            }
+
+            eventListeners[eventType].Add(listener);
+        }
+
+        public void unregisterListener()
+        {
+            //TODO
+        }
+
+        public void TriggerEvent(EventInfo e)
+        {
+            System.Type eventType = e.GetType();
+            if (eventListeners == null || eventListeners[eventType] == null)
+            {
+                //No one is listening for this event
+                return;
+            }
+
+            foreach (EventListener listener in eventListeners[eventType])
+            {
+                listener(e);
+            }
+        }
+    }
+}
