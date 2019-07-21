559b40eb6bb85581866da1953dd8cd364d06115c
Scott Duman <sbd39@comcast.net>
2019-07-21 05:30:16 -0700

    Improved Player Movement
    I got rid of the character controller on the player and instead decided to just use a rigid body by altering its velocity.

--- Capstone Prototype 1/Assets/Scripts/Player/PlayerMovement.cs
+++ Capstone Prototype 1/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,37 +2,54 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5;
 
     public float jumpForce = 5;
+    public float drag = 100;
 
     private Vector3 moveDir;
-    private CharacterController controller;
+    private Rigidbody body;
+    private CapsuleCollider collider;
 
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        body = GetComponent<Rigidbody>();
+        collider = GetComponent<CapsuleCollider>();
         moveDir = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float y = moveDir.y;
 
-        moveDir = transform.right * Input.GetAxis(InputManager.HORIZONTAL);
-        moveDir += transform.forward * Input.GetAxis(InputManager.VERTICAL);
-        moveDir = moveDir.normalized * moveSpeed;
+        moveDir = new Vector3(Input.GetAxis(InputManager.HORIZONTAL), 0, Input.GetAxis(InputManager.VERTICAL));
 
-        if (controller.isGrounded)
-        { 
-            if (Input.GetAxis(InputManager.JUMP) > 0) moveDir.y = jumpForce;
+        if (moveDir.magnitude > 0)
+        {
+            moveDir = transform.rotation * moveDir;
+            moveDir = moveDir.normalized * moveSpeed;
+            moveDir.y = body.velocity.y;
+
+            body.velocity = moveDir;
         }
-        else moveDir.y = y + Physics.gravity.y*Time.deltaTime;
+        else if (body.velocity.magnitude > 0)
+        {
+            ApplyDrag();
+        }
 
-        controller.Move(moveDir * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position - new Vector3(0, collider.height/2, 0), Vector3.down, 0.1f))
+        {
+            body.velocity = new Vector3(body.velocity.x, jumpForce, body.velocity.z);
+        }
+    }
+
+    private void ApplyDrag()
+    {
+        body.velocity = new Vector3(body.velocity.x * (1 - drag*Time.deltaTime), body.velocity.y, body.velocity.z * (1 - drag*Time.deltaTime));
     }
 }
