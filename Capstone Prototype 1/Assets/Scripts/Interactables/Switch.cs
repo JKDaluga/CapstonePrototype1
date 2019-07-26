0ab9349bd461ebfb3d3cf1e4bc89a91245dd1704
nmcnaughton <35637196+nmcnaughton@users.noreply.github.com>
2019-07-25 23:43:49 -0500

    BUGGED - switch
    Added a barebones switch, which can recieve interactions using leftclick. Bugged, cannot send events to Current. Need some help.

---
+++ Capstone Prototype 1/Assets/Scripts/Interactables/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour, IInteractable
+{
+    private bool SwitchState = false;
+
+    public EventCallbacks.toggleEvent t;
+
+    public virtual void Interact(GameObject player)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            print("RECIEVED");
+            if (!SwitchState)
+            {
+                SwitchState = true;
+                t = new EventCallbacks.toggleEvent(SwitchState, gameObject);
+                //EventCallbacks.EventSystem.Current.TriggerEvent(t);
+            }
+            else
+            {
+                SwitchState = false;
+                t = new EventCallbacks.toggleEvent(SwitchState, gameObject);
+                //EventCallbacks.EventSystem.Current.TriggerEvent(t);
+            }
+        }
+    }
+}
