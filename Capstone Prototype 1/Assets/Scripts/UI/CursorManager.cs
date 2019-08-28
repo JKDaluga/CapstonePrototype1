fd6744a8c7f27d18679549dd81fb58e8fbc92d69
JKDaluga <31351049+JKDaluga@users.noreply.github.com>
2019-08-27 22:25:52 -0500

    UI for state changes

--- Capstone Prototype 1/Assets/Scripts/UI/CursorManager.cs
+++ Capstone Prototype 1/Assets/Scripts/UI/CursorManager.cs
@@ -7,6 +7,11 @@ public class CursorManager : MonoBehaviour
 {
     Dictionary<string, Sprite>  cursorImages;
     private Image crossHairs;
+
+    public GameObject IceIcon;
+    public GameObject WaterIcon;
+    public GameObject WaterIcon1;
+    public GameObject SteamIcon;
     // Start is called before the first frame update
 
     void Start()
@@ -21,10 +26,42 @@ public class CursorManager : MonoBehaviour
         if (obj == null || obj.GetComponent<IInteractable>() == null)
         {
             crossHairs.color = Color.white;
+            IceIcon.SetActive(false);
+            WaterIcon.SetActive(false);
+            WaterIcon1.SetActive(false);
+            SteamIcon.SetActive(false);
         }
         else
         {
             crossHairs.color = Color.yellow;
+            if (obj.tag == "Ice")
+            {
+                IceIcon.SetActive(false);
+                WaterIcon.SetActive(false);
+                WaterIcon1.SetActive(true);
+                SteamIcon.SetActive(false);
+            }
+            else if(obj.tag == "water")
+            {
+                IceIcon.SetActive(true);
+                WaterIcon.SetActive(false);
+                WaterIcon1.SetActive(false);
+                SteamIcon.SetActive(true);
+            }
+            else if(obj.tag == "Steam")
+            {
+                IceIcon.SetActive(false);
+                WaterIcon.SetActive(true);
+                WaterIcon1.SetActive(false);
+                SteamIcon.SetActive(false);
+            }
+            else
+            {
+                IceIcon.SetActive(false);
+                WaterIcon.SetActive(false);
+                WaterIcon1.SetActive(false);
+                SteamIcon.SetActive(false);
+            }
         }
     }
 }
